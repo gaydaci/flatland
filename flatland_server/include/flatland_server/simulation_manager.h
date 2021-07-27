778368ea5a90ac9a4501508b36d70a995858e65e
Ducanor <duc.pichel@gmail.com>
2021-07-27 16:03:39 +0200

    layer loading works now

--- flatland_server/include/flatland_server/simulation_manager.h
+++ flatland_server/include/flatland_server/simulation_manager.h
@@ -111,9 +111,7 @@ class SimulationManager {
   bool callback_StepWorld(flatland_msgs::StepWorld::Request &request,
                           flatland_msgs::StepWorld::Response &response);
 
-  // void callback(nav_msgs::OccupancyGrid msg);  
-  // void callback(geometry_msgs::PoseStamped msg);
-  void callback(const std_msgs::String::ConstPtr& msg);
+  void callback(nav_msgs::OccupancyGrid msg);
 
 };
 };      // namespace flatland_server
